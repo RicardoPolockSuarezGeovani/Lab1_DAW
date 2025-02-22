626a031629a437a2e4c9be3fa62b5a508407061e
RicardoPolockSuarezGeovani <rsuarezg2301@alumno.ipn.mx>
2025-02-22 00:16:44 -0600

    Add files via upload

---
+++ insertar.php
@@ -0,0 +1,16 @@
+<?php
+include 'conexion.php';
+
+$nombre = "Juan Pérez";
+$correo = "juanperez@email.com";
+
+$sql = "INSERT INTO usuarios(nombre, correo) VALUES ('$nombre', '$correo')";
+
+if ($conexion->query($sql) === TRUE) {
+	echo "Usuario registrado correctamente.";
+} else {
+	echo "Error: " . $conexion->error;
+}
+
+$conexion->close();
+?>
